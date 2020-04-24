dc7c33ec73a6287680cab9d238e02af28055f6ae
Luis Miranda <luyimiranda07@gmail.com>
2020-04-23 21:11:23 -0500

    Create functions for show questions and shuffle array for options. Minor style fixes

--- trivia-app/dist/js/index.js
+++ trivia-app/dist/js/index.js
@@ -15,7 +15,7 @@ const infoAPI = {
     difficulty: "",
 };
 
-let numPregunta = 1;
+let numQuestion = 0;
 let totalCorrect = 0;
 
 // * Functions
@@ -37,6 +37,7 @@ async function getQuestions() {
 
     const questions = data.results.map((result) => {
         return {
+            category: result.category,
             question: result.question,
             incorrectOpt: result.incorrect_answers,
             correctOpt: result.correct_answer,
@@ -47,10 +48,44 @@ async function getQuestions() {
 }
 
 function showQuestions(questions) {
-    let questionEl = `
+    const actQuestion = questions[numQuestion];
+    let options = [...actQuestion.incorrectOpt, actQuestion.correctOpt];
+    options = optionsShuffle(options);
 
+    let questionEl = `
+        <small>${numQuestion + 1}/<span class="question-num">10</span></small>
+        <h3 class="subtitle">${actQuestion.category}</h3>
+        <p>${actQuestion.question}</p>
+        <form class="options">
+            <div><input type="radio" name="option" id="opt-1" class="option" checked><label for="opt-1">${options[0]}</label></input></div>
+            <div><input type="radio" name="option" id="opt-2" class="option"><label for="opt-2">${options[1]}</label></input></div>
+            <div><input type="radio" name="option" id="opt-3" class="option"><label for="opt-3">${options[2]}</label></input></div>
+            <div><input type="radio" name="option" id="opt-4" class="option"><label for="opt-4">${options[3]}</label></input></div>
+            <input type="submit" class="next-btn" id="next-btn" onsubmit="${nextQuestion}" value="Next">
+        </form>
     `;
     questionsSection.innerHTML = questionEl;
+
+
+}
+
+function checkAnswer() {
+
+}
+
+function nextQuestion() {
+
+}
+
+function optionsShuffle(array) {
+    for(let i = array.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * i);
+        const aux = array[i];
+        array[i] = array[j];
+        array[j] = aux;
+    }
+
+    return array;
 }
 
 // * Event listeners
@@ -70,6 +105,7 @@ for (const category of catsBtns) {
 for (const diff of diffBtns) {
     diff.addEventListener("click", () => {
         infoAPI.difficulty = event.target.dataset.diff;
+        displaySections(diffSection, questionsSection);
         getQuestions();
     });
 }
